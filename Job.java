83f1d3b612f8629ed1af563267d53919065e5dc8
PrinceWanted <56977987+PrinceWanted@users.noreply.github.com>
2020-05-06 23:22:42 +0200

    Job update with arraylist
    fck SE2

--- Job.java
+++ Job.java
@@ -23,7 +23,7 @@ public class Job implements Job_Read_Only{
     private int ExprienceNeeded;
     private String CareerLevel;
     public static ArrayList<Job> AllJobs;
-    public  ArrayList <Application>ApplicationList;
+    public  ArrayList <Application> ApplicationList;
 
     
 
